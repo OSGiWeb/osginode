8ffbdab67192f6b685f9a67918207b066750dcc7
Yun Xu <68616564@qq.com>
2016-04-03 11:32:00 +0800

    Change some routes config to avoid old version of react-router warning

--- app/Router.jsx
+++ app/Router.jsx
@@ -34,40 +34,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, browserHistory, createMemoryHistory } from 'react-router';
+import { Router, browserHistory, useRouterHistory  } from 'react-router';
+import { createHashHistory } from 'history'
 import { syncHistoryWithStore } from 'react-router-redux';
 import createRoutes from './routes.jsx';
 import configureStore from 'store/configureStore';
-import History from './components/smartAdmin/layout/navigation/classes/History.js';
 
+// Using new browserHistory reference
+// const initialState = window.__INITIAL_STATE__;
+// const store = configureStore(initialState, browserHistory);
+// const history = syncHistoryWithStore(browserHistory, store);
+// const routes = createRoutes(store);
 
-// Grab the state from a global injected into
-// server-generated HTML
+// useRouterHistory creates a composable higher-order function
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 const initialState = window.__INITIAL_STATE__;
-const store = configureStore(initialState, History);
-
-// const authenticated = false;
-// const store = configureStore({
-//   user: {
-//     authenticated: authenticated,
-//     isWaiting: false,
-//     message: '',
-//     isLogin: true
-//   }
-// }, History);
-const history = syncHistoryWithStore(History, store);
+const store = configureStore(initialState, appHistory);
+const history = syncHistoryWithStore(appHistory, store);
 const routes = createRoutes(store);
-
-// const history = createMemoryHistory();
-// const authenticated = true;
-// const store = configureStore({
-//   user: {
-//     authenticated: authenticated,
-//     isWaiting: false,
-//     message: '',
-//     isLogin: true
-//   }
-// }, history);
 
 // Router converts <Route> element hierarchy to a route config:
 // Read more https://github.com/rackt/react-router/blob/latest/docs/Glossary.md#routeconfig
